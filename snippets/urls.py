821f6dc169ac700f7142da6ce3cd4eb37c4c90cc
Christian <arcec.arg@gmail.com>
2017-02-04 21:00:41 -0300

    Part 3 fixed

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from snippets import views
+from . import views
 
 urlpatterns = [
-    url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
+    url(r'^snippets/$', views.SnippetList),
+    url(r'^snippets/(?P<pk>[0-9]+)$', views.SnippetDetail),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)

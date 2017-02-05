db9abe856b6b0ae224ff256482917cbfb0db7732
Christian <arcec.arg@gmail.com>
2017-02-04 21:28:03 -0300

    Part 3 fixing views

--- snippets/views.py
+++ snippets/views.py
@@ -7,6 +7,7 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
